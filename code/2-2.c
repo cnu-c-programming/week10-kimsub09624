2c025d7bba53b3f7d96e432713b95eb360620e23
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-05-14 19:08:32 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -16,8 +16,8 @@ union file_access_rights {
 int main() {
 	union file_access_rights some_file = {0};
 	some_file.rights.read = 1;
-	some.file.rights.write = 0;
-	some.file.rights.execute = 1;
+	some_file.rights.write = 0;
+	some_file.rights.execute = 1;
 	printf("current mode: %u\n", some_file.int_value);
 }
 
